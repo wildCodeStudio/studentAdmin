0931f6bb96c5d9bf563906e097bffd30a9616380
Your Name <125294303@qq.com>
2019-10-09 19:31:38 +0800

    sibo

---
+++ src/views/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-box-bs-002"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.home-box-bs-002 {
+  width: 100%;
+  height: 800px;
+  margin: 20px auto 0px;
+  background: url(../../assets/welcome_image/welcome.png) no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
