6d59084acdfc4ed8491f4a04e2106cb3bcd3520d
yemaStudio <yemastudio@163.com>
2019-10-14 11:37:44 +0800

    refactor:修改首页

--- src/router/index.js
+++ src/router/index.js
@@ -82,27 +82,6 @@ export const constantRoutes = [
       //   meta: { title: "Tree", icon: "tree" }
       // }
     ]
-  },
-  {
-    path: "/example",
-    component: Layout,
-    redirect: "/example/table",
-    name: "Example",
-    meta: { title: "Example", icon: "example" },
-    children: [
-      {
-        path: "table",
-        name: "Table",
-        component: () => import("@/views/table/index"),
-        meta: { title: "Table", icon: "table" }
-      },
-      {
-        path: "tree",
-        name: "Tree",
-        component: () => import("@/views/tree/index"),
-        meta: { title: "Tree", icon: "tree" }
-      }
-    ]
   }
 
 ]
